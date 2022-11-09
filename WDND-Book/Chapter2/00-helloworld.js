0a9615c9573b55a6fad5b0b34417c76f355f9032
Cumlord22 <thehiep320@gmail.com>
2022-11-09 23:49:23 +0700

    hiep js
    09/11

---
+++ WDND-Book/Chapter2/00-helloworld.js
@@ -0,0 +1,10 @@
+const http = require('http')
+const port = process.env.PORT || 3000
+const server = http.createServer((req, res) => {
+ res.writeHead(200, { 'Content-Type': 'text/html' })
+ res.end('Hi world!')
+
+})
+server.listen(port, () => console.log(`server started on
+port ${port}; ` +
+ 'Press Ctrl-C to terminate....'))
